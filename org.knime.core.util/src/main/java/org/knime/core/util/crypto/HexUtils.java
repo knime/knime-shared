f10dc6b051a96205c3d7dc694f3a1d1c15bd9c9d
Bernd Wiswedel <bernd.wiswedel@knime.com>
2011-10-20 12:16:22 +0000

    - added meta node locking (bug 2932) - added parts of save-as (still disabled)

---
+++ org.knime.core.util/src/main/java/org/knime/core/util/crypto/HexUtils.java
@@ -0,0 +1,94 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2003 - 2011
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME. The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
+ *
+ * History
+ *   Oct 19, 2011 (wiswedel): created
+ */
+package org.knime.core.util.crypto;
+
+/**
+ *
+ * @author Bernd Wiswedel, KNIME.com, Zurich, Switzerland
+ * @author Thorsten Meinl, University of Konstanz
+ */
+public final class HexUtils {
+
+    /** Hide. */
+    private HexUtils() {
+        // utility class
+    }
+
+    public static final char HEX_DIGIT[] = {'0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    public static String bytesToHex(final byte[] b) {
+        StringBuilder buf = new StringBuilder();
+        for (int j = 0; j < b.length; j++) {
+            buf.append(HEX_DIGIT[(b[j] >> 4) & 0x0f]);
+            buf.append(HEX_DIGIT[b[j] & 0x0f]);
+        }
+        return buf.toString();
+    }
+
+    public static byte[] hexToBytes(String hexString) {
+        if (hexString.length() % 2 != 0) {
+            hexString = "0" + hexString;
+        }
+
+        byte[] data = new byte[hexString.length() / 2];
+        for (int i = 0; i < hexString.length(); i += 2) {
+            int high = Character.digit(hexString.charAt(i), 16);
+            int low = Character.digit(hexString.charAt(i + 1), 16);
+            int value = (high << 4) | low;
+            if (value > 127) {
+                value -= 256;
+            }
+            data[i / 2] = (byte)value;
+        }
+        return data;
+    }
+}
