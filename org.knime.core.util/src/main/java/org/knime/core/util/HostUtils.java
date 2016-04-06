10b97cca8750a5974e78f2b6c1f73175ea155024
Thorsten Meinl <thorsten.meinl@knime.com>
2016-04-06 08:18:25 +0000

    return only the host's IPv4 addresses for now because the license generator cannot handle IPv6 addresses yet

--- org.knime.core.util/src/main/java/org/knime/core/util/HostUtils.java
+++ org.knime.core.util/src/main/java/org/knime/core/util/HostUtils.java
@@ -52,6 +52,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -165,14 +166,14 @@ public final class HostUtils {
                 for (NetworkInterface iface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
                     for (NetworkInterface viface : Collections.list(iface.getSubInterfaces())) {
                         for (InetAddress vaddr : Collections.list(viface.getInetAddresses())) {
-                            if (!isLocalAddress(vaddr)) {
+                            if (!isLocalAddress(vaddr) && (vaddr instanceof Inet4Address)) {
                                 temp.add(vaddr);
                             }
                         }
                     }
 
                     for (InetAddress raddr : Collections.list(iface.getInetAddresses())) {
-                        if (!isLocalAddress(raddr)) {
+                        if (!isLocalAddress(raddr) && (raddr instanceof Inet4Address)) {
                             temp.add(raddr);
                         }
                     }
