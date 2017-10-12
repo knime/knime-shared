f5dc118a42db9c1fd1af15859eb7fa242d93bc53
Thorsten Meinl <thorsten.meinl@knime.com>
2017-10-12 16:25:10 +0200

    SRV-1054: Added RandomInputStream for testing purposes
    SRV-1054 (Improve file transfer between client and executor)
    https://knime-com.atlassian.net/browse/SRV-1054

---
+++ org.knime.core.util/src/main/java/org/knime/core/util/RandomInputStream.java
@@ -0,0 +1,86 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Created on 12.10.2017 by thor
+ */
+package org.knime.core.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+/**
+ * Input stream that creates random bytes. This is probably only useful for testing.
+ *
+ * @author Thorsten Meinl, KNIME.com, Zurich, Switzerland
+ */
+public class RandomInputStream extends InputStream {
+    private final Random m_rand = new Random();
+
+    private long m_size;
+
+    /**
+     * Creates a new stream.
+     *
+     * @param size the number of bytes that this stream should deliver
+     */
+    public RandomInputStream(final int size) {
+        m_size = size;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int read() throws IOException {
+        if (m_size-- <= 0) {
+            return -1;
+        }
+        return m_rand.nextInt() & 0xff;
+    }
+
+    // if you really need larger amounts of data this implementation is inefficient. Consider implementing
+    // the other read methods, too.
+}
