91d836381b289a7ceda718daada17d2a7638f2c4
Alison Walter <alison.walter@knime.com>
2018-10-29 13:33:02 +0100

    AP-10303: Add pojos equivalent to those in knime-core
    These will replace the ones in knime-core.
    
    AP-10303 (API: "Workflowalizer" a slim parser for Workflows (will be
          used to feed Workflow Search Index))

---
+++ org.knime.core.util/src/main/java/org/knime/core/util/workflowalizer/AuthorInformation.java
@@ -0,0 +1,166 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Sep 12, 2018 (awalter): created
+ */
+package org.knime.core.util.workflowalizer;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * Meta data such as who create the workflow and who edited it last and when.
+ *
+ * <p>Extracted from KNIME core WorkflowManager.</p>
+ *
+ * @since 2.8
+ */
+public final class AuthorInformation {
+
+    /** Info for workflows created prior 2.8. */
+    public static final AuthorInformation UNKNOWN = new AuthorInformation("<unknown>", new Date(0), Optional.empty(), Optional.empty());
+
+    private final String m_author;
+
+    private final Date m_authoredDate;
+
+    private final Optional<String> m_lastEditor;
+
+    private final Optional<Date> m_lastEditDate;
+
+    /**
+     * Creates AuthorInformatio using "user.name" property as author and current system time as authored date. The edit
+     * fields are empty.
+     */
+    public AuthorInformation() {
+        this(System.getProperty("user.name"), new Date(), Optional.empty(), Optional.empty());
+    }
+
+    /**
+     * @param past used to provide author name and authored date, edit fields use system properties
+     */
+    public AuthorInformation(final AuthorInformation past) {
+        this(past.m_author, past.m_authoredDate, System.getProperty("user.name"), new Date());
+    }
+
+    /**
+     * @param author Original author.
+     * @param authoredDate Original authored date.
+     * @param lastEditor Name of last editor.
+     * @param lastEditDate Date of last edit.
+     */
+    public AuthorInformation(final String author, final Date authoredDate, final String lastEditor,
+        final Date lastEditDate) {
+        this(author, authoredDate, Optional.ofNullable(lastEditor), Optional.ofNullable(lastEditDate));
+    }
+
+    AuthorInformation(final String author, final Date authoredDate, final Optional<String> lastEditor,
+        final Optional<Date> lastEditDate) {
+        m_author = author;
+        m_authoredDate = authoredDate;
+        m_lastEditor = lastEditor;
+        m_lastEditDate = lastEditDate;
+    }
+
+    /** @return Name of the workflow author (person). Null when not saved yet. */
+    public String getAuthor() {
+        return m_author;
+    }
+
+    /** @return Date when the workflow was saved the first time. Can be null. */
+    public Date getAuthoredDate() {
+        return m_authoredDate;
+    }
+
+    /** @return Name of the person who edited the workflow last (on last save). Null when not saved yet. */
+    public Optional<String> getLastEditor() {
+        if (m_lastEditor == null) {
+            throw new UnsupportedOperationException("getLastEditor() is not supported, field was not read");
+        }
+        return m_lastEditor;
+    }
+
+    /** @return Date when workflow was saved last. */
+    public Optional<Date> getLastEditDate() {
+        if (m_lastEditDate == null) {
+            throw new UnsupportedOperationException("getLastEditDate() is not supported, field was not read");
+        }
+        return m_lastEditDate;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof AuthorInformation)) {
+            return false;
+        }
+        final AuthorInformation other = (AuthorInformation) obj;
+        return new EqualsBuilder()
+                .append(m_author, other.m_author)
+                .append(m_authoredDate, other.m_authoredDate)
+                .append(m_lastEditor, other.m_lastEditor)
+                .append(m_lastEditDate, other.m_lastEditDate)
+                .isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(m_author)
+                .append(m_authoredDate)
+                .append(m_lastEditor)
+                .append(m_lastEditDate)
+                .toHashCode();
+    }
+}
---
+++ org.knime.core.util/src/main/java/org/knime/core/util/workflowalizer/NodeAndBundleInformation.java
@@ -0,0 +1,271 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Sep 12, 2018 (awalter): created
+ */
+package org.knime.core.util.workflowalizer;
+
+import java.util.Optional;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.knime.core.util.Version;
+
+/**
+ * POJO containing information about a KNIME node and bundle information.
+ *
+ * <p>Adapted from NodeAndBundleInformation class in knime-core.</p>
+ */
+public class NodeAndBundleInformation {
+
+    private final Optional<String> m_featureSymbolicName;
+    private final Optional<String> m_featureName;
+    private final Optional<String> m_featureVendor;
+    private final Optional<Version> m_featureVersion;
+    private final Optional<String> m_bundleSymbolicName;
+    private final Optional<String> m_bundleName;
+    private final Optional<String> m_bundleVendor;
+    private final Optional<String> m_nodeName;
+    private final Optional<String> m_factoryClass;
+    private final Optional<Version> m_bundleVersion;
+
+    /**
+     *
+     * @param factoryClass
+     * @param bundleSymbolicName
+     * @param bundleName
+     * @param bundleVendor
+     * @param nodeName
+     * @param bundleVersion
+     * @param featureSymbolicName
+     * @param featureName
+     * @param featureVendor
+     * @param featureVersion
+     */
+    public NodeAndBundleInformation(final Optional<String> factoryClass, final Optional<String> bundleSymbolicName,
+        final Optional<String> bundleName, final Optional<String> bundleVendor, final Optional<String> nodeName,
+        final Optional<Version> bundleVersion, final Optional<String> featureSymbolicName,
+        final Optional<String> featureName, final Optional<String> featureVendor,
+        final Optional<Version> featureVersion) {
+        m_bundleSymbolicName = bundleSymbolicName;
+        m_bundleName = bundleName;
+        m_bundleVendor = bundleVendor;
+        m_nodeName = nodeName;
+        m_bundleVersion = bundleVersion;
+        m_featureName = featureName;
+        m_featureSymbolicName = featureSymbolicName;
+        m_featureVendor = featureVendor;
+        m_featureVersion = featureVersion;
+        m_factoryClass = factoryClass;
+    }
+
+    /**
+     * Returns the bundle's name in which the node is contained.
+     *
+     * @return the bundle's name
+     * @since 3.0
+     */
+    public Optional<String> getBundleName() {
+        if (m_bundleName == null) {
+            throw new UnsupportedOperationException("getBundleName() is not supported, field was not read");
+        }
+        return m_bundleName;
+    }
+
+    /**
+     * Returns the bundle's symbolic name in which the component is contained.
+     *
+     * @return the bundle's symbolic name
+     */
+    public Optional<String> getBundleSymbolicName() {
+        if (m_bundleSymbolicName == null) {
+            throw new UnsupportedOperationException("getBundleSymbolicName() is not supported, field was not read");
+        }
+        return m_bundleSymbolicName;
+    }
+
+    /**
+     * Returns the bundle's vendor in which the node is contained.
+     *
+     * @return the bundle's vendor
+     * @since 3.0
+     */
+    public Optional<String> getBundleVendor() {
+        if (m_bundleVendor == null) {
+            throw new UnsupportedOperationException("getBundleVendor() is not supported, field was not read");
+        }
+        return m_bundleVendor;
+    }
+
+    /**
+     * Returns the bundle's version in which the node is contained.
+     *
+     * @return the bundle's version
+     * @since 3.0
+     */
+    public Optional<Version> getBundleVersion() {
+        if (m_bundleVersion == null) {
+            throw new UnsupportedOperationException("getBundleVersion() is not supported, field was not read");
+        }
+        return m_bundleVersion;
+    }
+
+    /**
+     * Returns the node's name.
+     *
+     * @return the node's name
+     * @since 3.0
+     */
+    public Optional<String> getNodeName() {
+        if (m_nodeName == null) {
+            throw new UnsupportedOperationException("getNodeName() is not supported, field was not read");
+        }
+        return m_nodeName;
+    }
+
+    /**
+     * Returns the node's factory class name.
+     *
+     * @return the factory class, never <code>null</code>
+     */
+    public Optional<String> getFactoryClass() {
+        if (m_factoryClass == null) {
+            throw new UnsupportedOperationException("getFactoryClass() is not supported, field was not read");
+        }
+        return m_factoryClass;
+    }
+
+    /**
+     * Returns the features's name in which the node is contained.
+     *
+     * @return the feature's name
+     * @since 3.0
+     */
+    public Optional<String> getFeatureName() {
+        if (m_featureName == null) {
+            throw new UnsupportedOperationException("getFeatureName() is not supported, field was not read");
+        }
+        return m_featureName;
+    }
+
+    /**
+     * Returns the features's symbolic name in which the component is contained.
+     *
+     * @return the feature's symbolic name
+     */
+    public Optional<String> getFeatureSymbolicName() {
+        if (m_featureSymbolicName == null) {
+            throw new UnsupportedOperationException("getFeatureSymbolicName() is not supported, field was not read");
+        }
+        return m_featureSymbolicName;
+    }
+
+    /**
+     * Returns the features' vendor in which the node is contained.
+     *
+     * @return the feature's vendor
+     * @since 3.0
+     */
+    public Optional<String> getFeatureVendor() {
+        if (m_featureVendor == null) {
+            throw new UnsupportedOperationException("getFeatureVendor() is not supported, field was not read");
+        }
+        return m_featureVendor;
+    }
+
+    /**
+     * Returns the features's version in which the node is contained.
+     *
+     * @return the feature's version
+     * @since 3.0
+     */
+    public Optional<Version> getFeatureVersion() {
+        if (m_featureVersion == null) {
+            throw new UnsupportedOperationException("getFeatureVersion() is not supported, field was not read");
+        }
+        return m_featureVersion;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof NodeAndBundleInformation)) {
+            return false;
+        }
+        final NodeAndBundleInformation other = (NodeAndBundleInformation)obj;
+        return new EqualsBuilder()
+                .append(m_featureSymbolicName, other.m_featureSymbolicName)
+                .append(m_featureName, other.m_featureName)
+                .append(m_featureVendor, other.m_featureVendor)
+                .append(m_featureVersion, other.m_featureVersion)
+                .append(m_bundleSymbolicName, other.m_bundleSymbolicName)
+                .append(m_bundleName, other.m_bundleName)
+                .append(m_bundleVendor, other.m_bundleVendor)
+                .append(m_nodeName, other.m_nodeName)
+                .append(m_factoryClass, other.m_factoryClass)
+                .append(m_bundleVersion, other.m_bundleVersion)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(m_featureSymbolicName)
+                .append(m_featureName)
+                .append(m_featureVendor)
+                .append(m_featureVersion)
+                .append(m_bundleSymbolicName)
+                .append(m_bundleName)
+                .append(m_bundleVendor)
+                .append(m_nodeName)
+                .append(m_factoryClass)
+                .append(m_bundleVersion)
+                .toHashCode();
+    }
+}
+
