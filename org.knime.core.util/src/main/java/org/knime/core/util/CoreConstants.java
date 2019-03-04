c3c5a899f18c5589b07f394ac99bb7e6326fe002
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2019-03-04 17:13:44 +0100

    AP-11261: added CoreConstants class (non-API)
    AP-11261 ((API) Add hook to workflow loading routine to populate
    "knime.system.credentials" )

---
+++ org.knime.core.util/src/main/java/org/knime/core/util/CoreConstants.java
@@ -0,0 +1,73 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Feb 7, 2019 (wiswedel): created
+ */
+package org.knime.core.util;
+
+/**
+ * Constants used in the KNIME framework. Constants are shared in the KNIME AP code and other,
+ * non-OSGI components (server).
+ *
+ * <p>Class and fields are not meant as public API and can change between releases.
+ *
+ * @noreference This class is not intended to be referenced by clients.
+ *
+ * @author Bernd Wiswedel, KNIME AG, Zurich, Switzerland
+ *
+ * @since 5.11
+ */
+public final class CoreConstants {
+
+    private CoreConstants() {
+    }
+
+    /**
+     * Identifier of a credentials object whose user/pass field should be pre-filled with the login information.
+     * Currently used when the user logs in on the server web portal and in API hooks in KNIME AP (AP-11261).
+     */
+    public static final String CREDENTIALS_KNIME_SYSTEM_DEFAULT_ID = "knime.system.default";
+}
