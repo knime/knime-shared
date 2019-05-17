a4aa8bc9f12fb8be11d06b8c780ea96d115f2e13
Jason Tyler <jason.tyler@knime.com>
2019-05-17 11:10:42 +0200

    Add missing import

--- org.knime.core.util/src/main/java/org/knime/core/util/workflowalizer2/ObjectStore.java
+++ org.knime.core.util/src/main/java/org/knime/core/util/workflowalizer2/ObjectStore.java
@@ -1,5 +1,7 @@
 package org.knime.core.util.workflowalizer2;
 
+import java.io.File;
+
 public interface ObjectStore {
 
     public String saveObject(File file, String name, String path, String workflowId);
