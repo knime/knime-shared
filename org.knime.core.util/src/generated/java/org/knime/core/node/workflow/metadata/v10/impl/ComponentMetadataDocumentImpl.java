fa69616d17f1ce90dcf7e05e8a15e1165a38288b
Leonard Wörteler <leonard.woerteler@knime.com>
2023-05-12 11:36:59 +0200

    AP-20406: New metadata XML file format
    AP-20406 (WorkflowMetaData API (near WorkflowDef))

---
+++ org.knime.core.util/src/generated/java/org/knime/core/node/workflow/metadata/v10/impl/ComponentMetadataDocumentImpl.java
@@ -0,0 +1,68 @@
+/*
+ * An XML document type.
+ * Localname: component-metadata
+ * Namespace: http://www.knime.org/core/node/workflow/metadata/v1.0
+ * Java type: org.knime.core.node.workflow.metadata.v10.ComponentMetadataDocument
+ *
+ * Automatically generated - do not modify.
+ */
+package org.knime.core.node.workflow.metadata.v10.impl;
+
+import javax.xml.namespace.QName;
+import org.apache.xmlbeans.QNameSet;
+import org.apache.xmlbeans.XmlObject;
+
+/**
+ *
+ * @since 6.0
+ *
+ * A document containing one component-metadata(@http://www.knime.org/core/node/workflow/metadata/v1.0) element.
+ *
+ * This is a complex type.
+ */
+public class ComponentMetadataDocumentImpl extends org.apache.xmlbeans.impl.values.XmlComplexContentImpl implements org.knime.core.node.workflow.metadata.v10.ComponentMetadataDocument {
+    private static final long serialVersionUID = 1L;
+
+    public ComponentMetadataDocumentImpl(org.apache.xmlbeans.SchemaType sType) {
+        super(sType);
+    }
+
+    private static final QName[] PROPERTY_QNAME = {
+        new QName("http://www.knime.org/core/node/workflow/metadata/v1.0", "component-metadata"),
+    };
+
+
+    /**
+     * @since 6.0
+     */
+    @Override
+    public org.knime.core.node.workflow.metadata.v10.ComponentMetadata getComponentMetadata() {
+        synchronized (monitor()) {
+            check_orphaned();
+            org.knime.core.node.workflow.metadata.v10.ComponentMetadata target = null;
+            target = (org.knime.core.node.workflow.metadata.v10.ComponentMetadata)get_store().find_element_user(PROPERTY_QNAME[0], 0);
+            return (target == null) ? null : target;
+        }
+    }
+
+    /**
+     * Sets the "component-metadata" element
+     */
+    @Override
+    public void setComponentMetadata(org.knime.core.node.workflow.metadata.v10.ComponentMetadata componentMetadata) {
+        generatedSetterHelperImpl(componentMetadata, PROPERTY_QNAME[0], 0, org.apache.xmlbeans.impl.values.XmlObjectBase.KIND_SETTERHELPER_SINGLETON);
+    }
+
+    /**
+     * Appends and returns a new empty "component-metadata" element
+     */
+    @Override
+    public org.knime.core.node.workflow.metadata.v10.ComponentMetadata addNewComponentMetadata() {
+        synchronized (monitor()) {
+            check_orphaned();
+            org.knime.core.node.workflow.metadata.v10.ComponentMetadata target = null;
+            target = (org.knime.core.node.workflow.metadata.v10.ComponentMetadata)get_store().add_element_user(PROPERTY_QNAME[0]);
+            return target;
+        }
+    }
+}
---
+++ org.knime.core.util/src/generated/java/org/knime/core/node/workflow/metadata/v10/impl/WorkflowMetadataDocumentImpl.java
@@ -0,0 +1,68 @@
+/*
+ * An XML document type.
+ * Localname: workflow-metadata
+ * Namespace: http://www.knime.org/core/node/workflow/metadata/v1.0
+ * Java type: org.knime.core.node.workflow.metadata.v10.WorkflowMetadataDocument
+ *
+ * Automatically generated - do not modify.
+ */
+package org.knime.core.node.workflow.metadata.v10.impl;
+
+import javax.xml.namespace.QName;
+import org.apache.xmlbeans.QNameSet;
+import org.apache.xmlbeans.XmlObject;
+
+/**
+ *
+ * @since 6.0
+ *
+ * A document containing one workflow-metadata(@http://www.knime.org/core/node/workflow/metadata/v1.0) element.
+ *
+ * This is a complex type.
+ */
+public class WorkflowMetadataDocumentImpl extends org.apache.xmlbeans.impl.values.XmlComplexContentImpl implements org.knime.core.node.workflow.metadata.v10.WorkflowMetadataDocument {
+    private static final long serialVersionUID = 1L;
+
+    public WorkflowMetadataDocumentImpl(org.apache.xmlbeans.SchemaType sType) {
+        super(sType);
+    }
+
+    private static final QName[] PROPERTY_QNAME = {
+        new QName("http://www.knime.org/core/node/workflow/metadata/v1.0", "workflow-metadata"),
+    };
+
+
+    /**
+     * @since 6.0
+     */
+    @Override
+    public org.knime.core.node.workflow.metadata.v10.WorkflowMetadata getWorkflowMetadata() {
+        synchronized (monitor()) {
+            check_orphaned();
+            org.knime.core.node.workflow.metadata.v10.WorkflowMetadata target = null;
+            target = (org.knime.core.node.workflow.metadata.v10.WorkflowMetadata)get_store().find_element_user(PROPERTY_QNAME[0], 0);
+            return (target == null) ? null : target;
+        }
+    }
+
+    /**
+     * Sets the "workflow-metadata" element
+     */
+    @Override
+    public void setWorkflowMetadata(org.knime.core.node.workflow.metadata.v10.WorkflowMetadata workflowMetadata) {
+        generatedSetterHelperImpl(workflowMetadata, PROPERTY_QNAME[0], 0, org.apache.xmlbeans.impl.values.XmlObjectBase.KIND_SETTERHELPER_SINGLETON);
+    }
+
+    /**
+     * Appends and returns a new empty "workflow-metadata" element
+     */
+    @Override
+    public org.knime.core.node.workflow.metadata.v10.WorkflowMetadata addNewWorkflowMetadata() {
+        synchronized (monitor()) {
+            check_orphaned();
+            org.knime.core.node.workflow.metadata.v10.WorkflowMetadata target = null;
+            target = (org.knime.core.node.workflow.metadata.v10.WorkflowMetadata)get_store().add_element_user(PROPERTY_QNAME[0]);
+            return target;
+        }
+    }
+}
