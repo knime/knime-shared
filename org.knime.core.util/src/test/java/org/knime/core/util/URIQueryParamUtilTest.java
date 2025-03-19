4b4c740b42d28848fb168ff34bd8ea59d086d8b3
Manuel Hotz <manuel.hotz@knime.com>
2025-03-19 17:22:26 +0100

    AP-24033: Add more unit tests
    AP-24033 (Remove knime-core dependency from knime-hubclient-sdk)

--- org.knime.core.util/src/test/java/org/knime/core/util/URIQueryParamUtilTest.java
+++ org.knime.core.util/src/test/java/org/knime/core/util/URIQueryParamUtilTest.java
@@ -44,74 +44,57 @@
  * ---------------------------------------------------------------------
  *
  * History
- *   18 Mar 2025 (Manuel Hotz, KNIME GmbH, Konstanz, Germany): created
+ *   19 Mar 2025 (Manuel Hotz, KNIME GmbH, Konstanz, Germany): created
  */
-package org.knime.core.util.hub;
+package org.knime.core.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import java.net.URI;
 
 import org.junit.jupiter.api.Test;
 
 /**
- * Tests for {@link ItemVersion} creation and handling.
+ * Tests for {@link URIQueryParamUtil}.
  *
  * @author Manuel Hotz, KNIME GmbH, Konstanz, Germany
  */
-final class ItemVersionTest {
+final class URIQueryParamUtilTest {
 
     @SuppressWarnings("static-method")
     @Test
-    void testSpecificVersionArguments() {
-        final var versionNumber = -1;
-        final var thrown = assertThrows(IllegalArgumentException.class, () -> new SpecificVersion(versionNumber),
-            "Expected to reject negative version number");
-        assertTrue(thrown.getMessage().contains("" + versionNumber), "Expected exception to contain version number");
-        assertEquals(0, new SpecificVersion(0).version(), "Expected version number to be 0");
-        assertEquals(1, ItemVersion.of(1).version(), "Expected version number to be 1");
+    void testAddQueryParam() {
+        final var uri = URI.create("https://example.com");
+        final var uriWithParam = URIQueryParamUtil.applyTo(uri, "key", "value");
+        assertEquals("https://example.com?key=value", uriWithParam.toString(),
+            "Expected URI to contain key-value pair as query params.");
     }
 
     @SuppressWarnings("static-method")
     @Test
-    void testVersionMatch() {
-        final var current = ItemVersion.currentState();
-        final var matchedCurrent = current.match(cs -> cs, mr -> fail("Unexpected most-recent version"),
-            sv -> fail("Unexpected specific version: " + sv.version()));
-        assertEquals(new CurrentState(), matchedCurrent, "Expected to match current state record");
-        assertFalse(current.isVersioned(), "Expected unversioned");
+    void testReplaceQueryParam() {
+        final var uri = URI.create("https://example.com?key=value");
+        final var uriWithParam = URIQueryParamUtil.applyTo(uri, "key", "new-value");
+        assertEquals("https://example.com?key=new-value", uriWithParam.toString(),
+            "Expected URI to updated key-value pair as query params.");
 
-        final var mostRecent = ItemVersion.mostRecent();
-        final var matchedMostRecent = mostRecent.match(cs -> fail("Unexpected current-state"), mr -> mr,
-            sv -> fail("Unexpected specific version: " + sv.version()));
-        assertEquals(new MostRecent(), matchedMostRecent, "Expected to match most recent record");
-        assertTrue(mostRecent.isVersioned(), "Expected is versioned");
-
-        final var specific = new SpecificVersion(42);
-        final var matchedSpecific = specific.match(cs -> fail("Unexpected current-state"),
-            mr -> fail("Unexpected most-recent version"), sv -> sv);
-        assertEquals(new SpecificVersion(42), matchedSpecific, "Expected to match specific version record");
-        assertEquals("42", matchedSpecific.getVersionString(), "Expected version string to be \"42\"");
-        assertTrue(specific.isVersioned(), "Expected is versioned");
+        final var uriWithNewParam = URIQueryParamUtil.applyTo(uri, "new-key", "new-value");
+        assertEquals("https://example.com?key=value&new-key=new-value", uriWithNewParam.toString(),
+            "Expected URI to contain new key-value pair as query params under new name.");
     }
 
     @SuppressWarnings("static-method")
     @Test
-    void testCast() {
-        // simple test for the "marker method"
-        assertEquals(ItemVersion.currentState(), ItemVersion.currentState().cast(CurrentState.class),
-            "Expected to cast to CurrentState");
-    }
+    void testException() {
+        final var uriWithCommaSeparated = URI.create("https://example.com?key=value,value2");
+        final var thrown = assertThrows(IllegalArgumentException.class,
+            () -> URIQueryParamUtil.applyTo(uriWithCommaSeparated, "key", "v"), "Expected method to throw IAE");
+        assertTrue(thrown.getMessage().contains("Cannot handle multi-valued query parameters"),
+            "Expected message to contain hint about multi-valued query parameters.");
 
-    @SuppressWarnings("static-method")
-    @Test
-    void testIdentifiers() {
-        assertEquals("most-recent", MostRecent.getIdentifier(),
-            "Expected \"most-recent\" for \"most-recent\" version record");
-        assertEquals("current-state", CurrentState.getIdentifier(),
-            "Expected \"current-state\" for \"current-state\" version record");
+        // IllegalStateException because of URISyntaxException should not happen, because we only modify the query
+        // parameters
     }
-
 }
